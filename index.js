546e57f2c5cb83407d42feb5593c84e819674c8d
aishabreena <aishabreena@gmail.com>
2024-12-04 18:15:51 +0700

    Update index.js
    add const port 5000

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require('./app') // the actual Express application
 const config = require('./utils/config.js')
 const logger = require('./utils/logger')
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
